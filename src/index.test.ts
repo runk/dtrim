d32b70fe0b669b260deb1677075c917b029ef12d
Zac Brydon <52645024+zbrydon@users.noreply.github.com>
2025-10-17 16:38:44 +1100

    Omit functions

--- src/index.test.ts
+++ src/index.test.ts
@@ -196,3 +196,39 @@ test('rule: #ignore', (t) => {
     c: foo,
   });
 });
+
+test('rule: #removeFunctions', (t) => {
+  const input = {
+    regularProp: 'value',
+    fn: () => 'function',
+    anotherProp: 42,
+    anotherFn: function namedFunction() {
+      return 'named';
+    },
+    nested: {
+      innerFn: () => 'inner',
+      innerProp: 'inner value',
+    },
+  };
+
+  const defaultOutput = trimmerFactory()(input);
+  t.deepEqual(defaultOutput, {
+    regularProp: 'value',
+    fn: '[Function]',
+    anotherProp: 42,
+    anotherFn: '[Function]',
+    nested: {
+      innerFn: '[Function]',
+      innerProp: 'inner value',
+    },
+  });
+
+  const removeFunctionsOutput = trimmerFactory({ functions: false })(input);
+  t.deepEqual(removeFunctionsOutput, {
+    regularProp: 'value',
+    anotherProp: 42,
+    nested: {
+      innerProp: 'inner value',
+    },
+  });
+});
